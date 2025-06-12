bd95e07b91beca20e1ff3b074cfb25eb0259f86a
victorsaccucci <victorsaccucciv@gmail.com>
2025-06-11 21:25:59 -0300

    first commit

---
+++ src/test/java/com/fiap/techchallenge/TechchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
