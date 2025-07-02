9627a605703a66fcc9fbd3a1e80b39854e85e126
Adriely Floriano <56102850+Adrielycfloriano@users.noreply.github.com>
2025-07-02 20:27:24 -0300

    feat: Implementando entidade de endereço com anotações JPA.

---
+++ src/main/java/com/fiap/techchallenge/model/Endereco.java
@@ -0,0 +1,22 @@
+package com.fiap.techchallenge.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "endereco")
+@Data
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua ;
+    private String bairro ;
+    private String cidade;
+    private String numero;
+    private String estado;
+    private String pais;
+
+}
