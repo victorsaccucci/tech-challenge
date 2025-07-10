da789593daccff9275b7c548cf3d3edf0f5c06ec
Caroline Barbara <caroline.sbarbara@gmail.com>
2025-07-10 18:14:06 -0300

    feat - adicionado o metodo trocarSenha

--- src/main/java/com/fiap/techchallenge/service/UsuarioService.java
+++ src/main/java/com/fiap/techchallenge/service/UsuarioService.java
@@ -1,9 +1,11 @@
 package com.fiap.techchallenge.service;
 
 import com.fiap.techchallenge.dto.AtualizarUsuarioDTO;
+import com.fiap.techchallenge.dto.TrocarSenhaDTO;
 import com.fiap.techchallenge.model.Usuario;
 import com.fiap.techchallenge.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,19 +14,28 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Usuario cadastrarUsuario(Usuario usuario, String login, String senha){
         return usuarioRepository.save(usuario);
     }
 
     public Usuario atualizarUsuario(Long id, AtualizarUsuarioDTO atualizarDTO){
         Usuario usuario = usuarioRepository.findById(id).orElseThrow();
-        
+
         usuario.setEmail(atualizarDTO.email());
         usuario.setLogin(atualizarDTO.login());
         usuario.setTelefone(atualizarDTO.telefone());
         usuario.setEndereco(atualizarDTO.endereco());
 
         return usuarioRepository.save(usuario);
+    }
+
+    public void trocarSenha(Long id, TrocarSenhaDTO senhaDTO) {
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
+        usuario.setSenha(passwordEncoder.encode(senhaDTO.novaSenha()));
+        usuarioRepository.save(usuario);
     }
 
 }
