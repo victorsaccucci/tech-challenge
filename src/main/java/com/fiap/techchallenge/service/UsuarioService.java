7f1ecc9e85d723734359a0e213ff6e64ba5d0280
Caroline Barbara <caroline.sbarbara@gmail.com>
2025-07-09 22:43:49 -0300

    feat - adicionado o metodo para atualizarUsuario

--- src/main/java/com/fiap/techchallenge/service/UsuarioService.java
+++ src/main/java/com/fiap/techchallenge/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.fiap.techchallenge.service;
 
+import com.fiap.techchallenge.dto.AtualizarUsuarioDTO;
 import com.fiap.techchallenge.model.Usuario;
 import com.fiap.techchallenge.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,17 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
 
     public Usuario cadastrarUsuario(Usuario usuario, String login, String senha){
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario atualizarUsuario(Long id, AtualizarUsuarioDTO atualizarDTO){
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
+        
+        usuario.setEmail(atualizarDTO.email());
+        usuario.setLogin(atualizarDTO.login());
+        usuario.setTelefone(atualizarDTO.telefone());
+        usuario.setEndereco(atualizarDTO.endereco());
+
         return usuarioRepository.save(usuario);
     }
 
