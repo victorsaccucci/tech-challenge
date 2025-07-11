29c4079a0d85d8ebe7d41ddb94bcefecbb03397f
Adriely Floriano <56102850+Adrielycfloriano@users.noreply.github.com>
2025-07-10 23:36:49 -0300

    refactor: simplificar métodos de atualização e deleção no UsuarioService e adicionar listagem de usuários.

--- src/main/java/com/fiap/zecomanda/service/UsuarioService.java
+++ src/main/java/com/fiap/zecomanda/service/UsuarioService.java
@@ -1,12 +1,13 @@
 package com.fiap.zecomanda.service;
 
-import com.fiap.zecomanda.dto.AtualizarUsuarioDto;
 import com.fiap.zecomanda.dto.TrocarSenhaDto;
 import com.fiap.zecomanda.entity.Usuario;
 import com.fiap.zecomanda.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -21,25 +22,27 @@ public class UsuarioService {
         return usuarioRepository.save(usuario);
     }
 
-    public Usuario atualizarUsuario(Long id, AtualizarUsuarioDto atualizarDTO) {
-        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
-
-        usuario.setEmail(atualizarDTO.email());
-        usuario.setLogin(atualizarDTO.login());
-        usuario.setTelefone(atualizarDTO.telefone());
-        usuario.setEndereco(atualizarDTO.endereco());
-
-        return usuarioRepository.save(usuario);
-    }
-
     public void trocarSenha(Long id, TrocarSenhaDto senhaDTO) {
         Usuario usuario = usuarioRepository.findById(id).orElseThrow();
         usuario.setSenha(passwordEncoder.encode(senhaDTO.novaSenha()));
         usuarioRepository.save(usuario);
     }
 
-    public void deletarUsuario(Long id) {
-        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
-        usuarioRepository.delete(usuario);
+    public void updateUsuario(Usuario usuario, Long id) {
+        var update = this.usuarioRepository.update(usuario, id);
+        if (update == 0) {
+            throw new RuntimeException("Usuário não encontrado");
+        }
+    }
+
+    public void delete(Long id) {
+        var delete = this.usuarioRepository.delete(id);
+        if (delete == 0) {
+            throw new RuntimeException("Usuário não encontrado");
+        }
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
     }
 }
