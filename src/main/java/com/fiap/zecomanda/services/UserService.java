0c5f42b32a9d7559db35e0c94324abe5499ce24a
Adriely Floriano <56102850+Adrielycfloriano@users.noreply.github.com>
2025-07-22 19:46:06 -0300

    refactor: renomeando UsuarioService para UserService.

--- src/main/java/com/fiap/zecomanda/services/UserService.java
+++ src/main/java/com/fiap/zecomanda/services/UserService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class UsuarioService {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
