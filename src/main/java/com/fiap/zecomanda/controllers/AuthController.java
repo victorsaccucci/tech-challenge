29da4693956aa84578effaf5da4c07138834cd15
Adriely Floriano <56102850+Adrielycfloriano@users.noreply.github.com>
2025-08-02 09:07:17 -0300

    refactor: separar lógica de autenticação e usuários em AuthController e UserController.

---
+++ src/main/java/com/fiap/zecomanda/controllers/AuthController.java
@@ -0,0 +1,56 @@
+package com.fiap.zecomanda.controllers;
+
+import com.fiap.zecomanda.common.config.swagger.openapi.controller.AuthApi;
+import com.fiap.zecomanda.dto.AuthenticationDTO;
+import com.fiap.zecomanda.dto.ChangePasswordDTO;
+import com.fiap.zecomanda.dto.LoginResponseDTO;
+import com.fiap.zecomanda.dto.RegisterUserDTO;
+import com.fiap.zecomanda.services.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class AuthController implements AuthApi {
+
+    private final AuthService authService;
+
+
+    public ResponseEntity<?> registerUser(@RequestBody RegisterUserDTO data) {
+        try {
+            authService.registerUser(data);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+    }
+
+    public ResponseEntity<?> login(@RequestBody AuthenticationDTO authBody) {
+        try {
+            LoginResponseDTO response = authService.login(authBody);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body((e.getMessage()));
+        }
+    }
+
+    public ResponseEntity<?> changePassword(
+            @RequestBody ChangePasswordDTO passwordDTO,
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        try {
+            authService.changePassword(passwordDTO, authorizationHeader);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
--- src/main/java/com/fiap/zecomanda/controllers/UserController.java
+++ src/main/java/com/fiap/zecomanda/controllers/UserController.java
@@ -1,111 +1,29 @@
 package com.fiap.zecomanda.controllers;
 
-import com.fiap.zecomanda.common.consts.UserRole;
-import com.fiap.zecomanda.common.consts.UserType;
-import com.fiap.zecomanda.common.security.TokenService;
-import com.fiap.zecomanda.dto.*;
-import com.fiap.zecomanda.entities.Address;
+import com.fiap.zecomanda.common.config.swagger.openapi.controller.UserApi;
+import com.fiap.zecomanda.common.config.swagger.openapi.dto.UserDtoApi;
+import com.fiap.zecomanda.dto.UpdateUserDTO;
 import com.fiap.zecomanda.entities.User;
-import com.fiap.zecomanda.repositories.UserRepository;
 import com.fiap.zecomanda.services.UserService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
-@RequestMapping("api/v1/user")
-@Tag(name = "User", description = "CRUD Controller for Users")
-public class UserController {
+@AllArgsConstructor
+public class UserController implements UserApi {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @Autowired
-    private TokenService tokenService;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody @Valid RegisterUserDTO data) {
-
-        if (userRepository.findByLoginAndPassword(data.login(), data.password()).isPresent()) {
-            return ResponseEntity.status(409).body("User already exists.");
-        }
-
-        String encodedPassword = new BCryptPasswordEncoder().encode(data.password());
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        String updatedAt = LocalDateTime.now().format(formatter);
-
-        UserRole typeUserRole = UserRole.USER;
-        Address address = data.address();
-
-        User newUser = new User(address, UserType.CUSTOMER, data.name(), data.email(), data.password(), encodedPassword, updatedAt,
-                data.login(), typeUserRole);
-
-        userService.registerUser(newUser);
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<List<UserDtoApi>> getUsers() {
+        return ResponseEntity.ok(userService.findAllUsers());
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthenticationDTO authBody) {
-        User user = this.userRepository.findByLogin(authBody.login())
-                .orElse(null);
-        if (user != null && passwordEncoder.matches(authBody.password(), user.getPassword())) {
-            String token = this.tokenService.generateToken(user);
-            return ResponseEntity.ok().body(new LoginResponseDTO(token, "Login succeeded."));
-        }
-        return ResponseEntity.status(401).body("Invalid credentials. Failed to login.");
-    }
-
-    @GetMapping
-    public ResponseEntity<List<User>> getUsers(@RequestParam("page") int page, @RequestParam("size") int size) {
-        var allUsers = this.userService.findAllUsers(page, size).getContent();
-        return ResponseEntity.ok(allUsers);
-    }
-
-    @PatchMapping("/change-password")
-    public ResponseEntity<?> changePassword(
-            @RequestBody ChangePasswordDTO passwordDTO,
-            @RequestHeader("Authorization") String authorizationHeader
-    ) {
-        Optional<User> foundUser = userService.extractUserSubject(authorizationHeader);
-
-        if (passwordDTO.currentPassword() == null || passwordDTO.currentPassword().isBlank()) {
-            return ResponseEntity.badRequest().body("Password is required.");
-        }
-
-        if (passwordDTO.newPassword() == null || passwordDTO.newPassword().isBlank()) {
-            return ResponseEntity.badRequest().body("New password is required.");
-        }
-
-        if (!passwordDTO.newPassword().equals(passwordDTO.confirmationPassword())) {
-            return ResponseEntity.badRequest().body("Password confirmation does not match.");
-        }
-
-        try {
-            userService.updatePassword(foundUser.get().getId(), passwordDTO);
-            return ResponseEntity.noContent().build();
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @PutMapping()
     public ResponseEntity<Void> updateUser(
             @RequestBody UpdateUserDTO user,
             @RequestHeader("Authorization") String authorizationHeader
@@ -115,8 +33,8 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping()
-    public ResponseEntity<Void> deleteUser(@RequestHeader("Authorization") String authorizationHeader
+    public ResponseEntity<Void> deleteUser(
+            @RequestHeader("Authorization") String authorizationHeader
     ) {
         Optional<User> foundUser = userService.extractUserSubject(authorizationHeader);
         this.userService.delete(foundUser.get().getId());
